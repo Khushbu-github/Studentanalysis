a973d4132997f8c25d606afe5ae98ccef55b8849
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-09-11 03:28:30 +0530

    add

--- database.py
+++ database.py
@@ -1,12 +1,34 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./studentanalysis.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Replace with your actual Neon DB connection string
+SQLALCHEMY_DATABASE_URL = os.getenv("DB")
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./studentanalysis.db"
+# Create engine (no connect_args for Postgres)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
